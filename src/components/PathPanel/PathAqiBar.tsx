ece9dd56e39212d148ebb225b3bb0d327009ff98
Joose Helle <joose.helle@gmail.com>
2020-11-21 17:49:34 +0200

    Adjust exposure chart style

--- src/components/PathPanel/PathAqiBar.tsx
+++ src/components/PathPanel/PathAqiBar.tsx
@@ -5,29 +5,53 @@ import { aqiColors } from '../../constants'
 const StyledAqiBar = styled.div<{ withMargins?: boolean }>`
   display: flex;
   width: 94%;
-  ${props => props.withMargins === true && css`
-    margin: 3px 0px 3px 0px;
-  `}
+  ${(props) =>
+    props.withMargins === true &&
+    css`
+      margin: 3px 0px 3px 0px;
+    `}
 `
-const StyledAqPc = styled.div<{ pc: number, aqiCl: AqiClass }>`
+const StyledAqPc = styled.div<{ first: boolean; last: boolean; pc: number; aqiCl: AqiClass }>`
   height: 8px;
-  box-shadow: 0 3px 5px 0 rgba(0,0,0,0.05), 0 3px 4px 0 rgba(0,0,0,0.01);
-  width: ${props => props.pc || '0'}%;
-  background-color: ${props => aqiColors[props.aqiCl] || 'grey'};
+  box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.05), 0 3px 4px 0 rgba(0, 0, 0, 0.01);
+  width: ${(props) => props.pc || '0'}%;
+  background-color: ${(props) => aqiColors[props.aqiCl] || 'grey'};
+  ${(props) =>
+    props.first &&
+    css`
+      border-radius: 3px 0px 0px 3px;
+    `}
+  ${(props) =>
+    props.last &&
+    css`
+      border-radius: 0px 3px 3px 0px;
+    `}
+  ${(props) =>
+    props.first &&
+    props.last &&
+    css`
+      border-radius: 3px;
+    `}
 `
 
 interface Props {
-  aqiPcts: { [key in AqiClass]: number },
+  aqiPcts: { [key in AqiClass]: number }
   withMargins?: boolean
 }
 
 export const PathAqiBar = ({ aqiPcts, withMargins }: Props) => {
   const aqiKeys = Object.keys(aqiPcts)
-  const aqis: AqiClass[] = aqiKeys.map(aqi => Number(aqi)).sort()
+  const aqis: AqiClass[] = aqiKeys.map((aqi) => Number(aqi)).sort()
 
   return (
     <StyledAqiBar withMargins={withMargins}>
-      {aqis.map(aqiCl => <StyledAqPc key={aqiCl} aqiCl={aqiCl} pc={aqiPcts[aqiCl]} />)}
+      {aqis.map((aqiCl, i) => {
+        const first = i === 0
+        const last = i === aqis.length - 1
+        return (
+          <StyledAqPc first={first} last={last} key={aqiCl} aqiCl={aqiCl} pc={aqiPcts[aqiCl]} />
+        )
+      })}
     </StyledAqiBar>
   )
 }
--- src/components/PathPanel/PathNoisesBar.tsx
+++ src/components/PathPanel/PathNoisesBar.tsx
@@ -5,29 +5,51 @@ import { dBColors } from '../../constants'
 const StyledNoiseBar = styled.div<{ withMargins?: boolean }>`
   display: flex;
   width: 94%;
-  ${props => props.withMargins === true && css`
-    margin: 3px 0px 3px 0px;
-  `}
+  ${(props) =>
+    props.withMargins === true &&
+    css`
+      margin: 3px 0px 3px 0px;
+    `}
 `
-const StyledNoisePc = styled.div<{ pc: number, dB: DbClass }>`
+const StyledNoisePc = styled.div<{ first: boolean; last: boolean; pc: number; dB: DbClass }>`
   height: 8px;
-  box-shadow: 0 3px 5px 0 rgba(0,0,0,0.05), 0 3px 4px 0 rgba(0,0,0,0.01);
-  width: ${props => props.pc || '0'}%;
-  background-color: ${props => dBColors[props.dB] || 'grey'};
+  box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.05), 0 3px 4px 0 rgba(0, 0, 0, 0.01);
+  width: ${(props) => props.pc || '0'}%;
+  background-color: ${(props) => dBColors[props.dB] || 'grey'};
+  ${(props) =>
+    props.first &&
+    css`
+      border-radius: 3px 0px 0px 3px;
+    `}
+  ${(props) =>
+    props.last &&
+    css`
+      border-radius: 0px 3px 3px 0px;
+    `}
+  ${(props) =>
+    props.first &&
+    props.last &&
+    css`
+      border-radius: 0px 3px 3px 0px;
+    `}
 `
 
 interface PathNoisesBarProps {
-  noisePcts: { [key in DbClass]: number },
+  noisePcts: { [key in DbClass]: number }
   withMargins?: boolean
 }
 
 export const PathNoisesBar = ({ noisePcts, withMargins }: PathNoisesBarProps) => {
   const dBKeys = Object.keys(noisePcts)
-  const dBs: DbClass[] = dBKeys.map(dB => Number(dB)).sort()
+  const dBs: DbClass[] = dBKeys.map((dB) => Number(dB)).sort()
 
   return (
     <StyledNoiseBar withMargins={withMargins}>
-      {dBs.map(dB => <StyledNoisePc key={dB} dB={dB} pc={noisePcts[dB]} />)}
+      {dBs.map((dB, i) => {
+        const first = i === 0
+        const last = i === dBs.length - 1
+        return <StyledNoisePc first={first} last={last} key={dB} dB={dB} pc={noisePcts[dB]} />
+      })}
     </StyledNoiseBar>
   )
 }
